b8313349e6ba3a57f224b7c62aeb5cc42e8731a8
naresh <nareshnalla567@gmail.com>
2020-07-07 11:26:44 +0530

    good code

---
+++ hashmap/src/com/nalla/Table.java
@@ -0,0 +1,15 @@
+package com.nalla;
+
+import java.util.HashMap;
+
+public class Table {
+	public static void main(String[] args) {
+	int a=10;
+	int b=4;
+	System.out.println();
+	System.out.println(a & b);
+		
+	}
+	
+
+}

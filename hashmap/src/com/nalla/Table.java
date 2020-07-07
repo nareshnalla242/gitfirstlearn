d7b7df8acbadd83b6ecb76c46c58a4de07e575cb
naresh <nareshnalla567@gmail.com>
2020-07-07 12:34:54 +0530

    see naresh

--- hashmap/src/com/nalla/Table.java
+++ hashmap/src/com/nalla/Table.java
@@ -6,7 +6,9 @@ public class Table {
 	public static void main(String[] args) {
 	int a=10;
 	int b=4;
-	
+	int c=9;
+	System.out.println(a+b+c);
+	System.out.println(a/b);
 	System.out.println(a );
 		
 	}
